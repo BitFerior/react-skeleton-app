d9f0ef63a6a80e33b4fe085b975039a030df17b4
Saeeda Khatoon <saeeda.khatoon230@gmail.com>
2023-04-15 21:13:53 +0500

    removed extra plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,14 @@
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common.js");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = merge(commonConfig, {
-  ...common,
   mode: "production",
   plugins: [
-    ...common.plugins,
+    ...commonConfig.plugins,
     new HtmlWebpackPlugin({
       //simplifies the creation of html files. if any assets are changed this plugin auto makes changes in html files
       template: "public/index.html",
     }),
-    new CompressionPlugin({
-      // it returns compressed files of all assets in bundle.js, even it gives a compressed bundle file. it reduces assets loading time
-      test: /\.js(\?.*)?$/i,
-      filename: "[path][query]",
-      algorithm: "gzip",
-      deleteOriginalAssets: false, //if it is true, original assets when they are compressed gets deleted and build uses compressed ones. it tries to save disk space
-    }),
+   
   ],
 });
