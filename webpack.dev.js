2d2cd564e46c03594ef04402da9a2f4526925acc
Misbah Khalil <misbah.khalil.az@gmail.com>
2023-04-26 23:51:41 +0500

    eslint config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,18 @@
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common.js");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
 module.exports = merge(commonConfig, {
   mode: "development",
   devtool: "inline-source-map", //  tells Webpack to include a source map as a data URI at the end of the JavaScript bundle file.
   devServer: {
     open: true,
+    hot: true,
   },
+  plugins: [
+    ...commonConfig.plugins,
+    new ESLintPlugin({
+      extensions: ["js", "jsx", "ts", "tsx"],
+    }),
+  ],
 });
