746c80a40d686efe3beccca0121ed5ff9038820c
Saeeda Khatoon <saeeda.khatoon230@gmail.com>
2023-05-13 11:30:16 +0500

    remove extra line

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import App from "./App";
 import ReactDOM from "react-dom/client";
-// import Search from "./components/Search";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
